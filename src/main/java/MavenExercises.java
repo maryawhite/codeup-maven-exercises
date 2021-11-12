30f1390d7a33f52076bddf2a36f4a18d7e0fee56
Mary White <mary.a.white28@gmail.com>
2021-11-12 12:34:36 -0500

    completed exercises

---
+++ src/main/java/MavenExercises.java
@@ -0,0 +1,20 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class MavenExercises {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, please enter a sentence.");
+        String userInput = scanner.nextLine();
+        System.out.println("You entered: " + userInput);
+        System.out.println("userInput is empty, true or false: " + StringUtils.isEmpty(userInput));
+        System.out.println("Your input reversed: " + StringUtils.reverse(userInput));
+        System.out.println("Your input is numeric true or false: " + StringUtils.isNumeric(userInput));
+        System.out.println("Your input with the case swapped: " + StringUtils.swapCase(userInput));
+
+    }
+
+
+}
