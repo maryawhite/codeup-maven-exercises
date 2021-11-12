b3da62201a186a68bbafae0b4976e58659f65996
Mary White <mary.a.white28@gmail.com>
2021-11-12 13:13:58 -0500

    testing other methods, center, and substringBetween

--- src/main/java/MavenExercises.java
+++ src/main/java/MavenExercises.java
@@ -8,12 +8,16 @@ public class MavenExercises {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, please enter a sentence.");
         String userInput = scanner.nextLine();
-        System.out.println("You entered: " + userInput);
         System.out.println("userInput is empty, true or false: " + StringUtils.isEmpty(userInput));
         System.out.println("Your input reversed: " + StringUtils.reverse(userInput));
         System.out.println("Your input is numeric true or false: " + StringUtils.isNumeric(userInput));
         System.out.println("Your input with the case swapped: " + StringUtils.swapCase(userInput));
         System.out.println("StringUtils.reverseDelimited(userInput, ',') = " + StringUtils.reverseDelimited(userInput, ','));
+        System.out.println("StringUtils.center(userInput, 20, 'T') = " + StringUtils.center(userInput, 20, '*'));
+
+        String myString = "Begin Failure is the greatest Teacher End";
+        System.out.println("StringUtils.substringBetween(myString, \"Begin\", \"End\") = " + StringUtils.substringBetween(myString, "Begin", "End"));
+
 
     }
 
